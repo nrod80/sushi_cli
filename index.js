3bbe2cf79358ff538a0f8522e582f7a41079e844
nrod80 <nicholas.rodriguez@duke.edu>
2016-10-04 11:05:49 -0500

    added options to use local data and local template

--- index.js
+++ index.js
@@ -4,42 +4,65 @@ const program = require('commander');
 const exec = require('child_process').exec;
 const chalk = require('chalk');
 const http = require('http');
-const sen = require('./templater/SENstack');
 
-const templater = (json_url) => {
-  // with more templates, check options to figure out which template to use
-  console.log(chalk.blue('finding your recipe...'));
-  http.get(json_url, function(res) {
-    res.on('data', function(chunk) {
-      console.log(chalk.blue('your sushi order has been placed!'));
-      const data = JSON.parse(chunk.toString()).json;
-      console.log(chalk.green('here\'s what your ordered:' + data.toString()));
+const templater = (options) => {
 
-      sen(data);
-      console.log(chalk.blue('step 1: grab the ingredients (npm install)'));
+  // define template
+  if (options.template) {
+    const template = require(options.template)
+    console.log(options.template)
+  } else if (options.sen) {
+    const template = require('./templater/SENstack');
+  } else {
+    return console.log(chalk.red('You must provide a template.'))
+  }
 
-      exec('cd ' + data.DB.def.DBname + '; npm install', (error, stdout, stderr) => {
-        if (error) console.log(error);
-        if (stdout) console.log(stdout);
-        if (stderr) console.log(stderr);
-        console.log(chalk.blue('step 2: roll the sushi (created database, starting server...)'));
-        console.log(chalk.green('step 3: enjoy! (server running on port 3000)'));
-        exec('cd ' + data.DB.def.DBname + '; npm start', (errornxt, stdoutnxt, stderrnxt) => {
-          if (error) console.log(errornxt);
-          if (stdout) console.log(stdoutnxt);
-          if (stderr) console.log(stderrnxt);
-        });
+  // get the data
+  if (options.url) {
+    console.log(chalk.blue('finding your recipe...'));
+    // get the json with an http request
+    http.get(json_url, function(res) {
+      // once data is received...
+      res.on('data', function(chunk) {
+        const data = JSON.parse(chunk.toString()).json;
+        cookRecipe(template, data)
       });
     });
-  });
+  } else if (options.json) {
+    const data = require(options.json)
+    cookRecipe(template, data)
+  } else {
+    console.log(chalk.red('You must provide a json.'))
+  }
 };
 
+function cookRecipe(template, data) {
+  console.log(chalk.blue('your sushi order has been placed!'));
+  console.log(chalk.green('here\'s what your ordered:' + data.toString()));
+  // call senstack on the received json
+  sen(data);
+  console.log(chalk.blue('step 1: grab the ingredients (npm install)'));
+  exec('cd ' + data.DB.def.DBname + '; npm install', (error, stdout, stderr) => {
+    if (error) console.log(error);
+    if (stdout) console.log(stdout);
+    if (stderr) console.log(stderr);
+    console.log(chalk.blue('step 2: roll the sushi (created database, starting server...)'));
+    console.log(chalk.green('step 3: enjoy! (server running on port 3000)'));
+    exec('cd ' + data.DB.def.DBname + '; npm start', (errornxt, stdoutnxt, stderrnxt) => {
+      if (error) console.log(errornxt);
+      if (stdout) console.log(stdoutnxt);
+      if (stderr) console.log(stderrnxt);
+    });
+  });
+}
+
+// defaults to json_url (optional req)
 program
   .version('0.0.1')
-  .command('cook <json_url>')
-  .option('-u, --url <json_url>', '')
-  .option('-p, --path <json_path>')
-  .option('-t, --template <path_to_custom_template>')
+  .command('cook')
+  .option('-u, --url <json_url>')
+  .option('-j, --json <json_path>')
+  .option('-t, --template <custom_template_path>')
   .option('--sen', 'use the senstack template')
   .description('cook the ingredients into a file system')
   .action(templater);
